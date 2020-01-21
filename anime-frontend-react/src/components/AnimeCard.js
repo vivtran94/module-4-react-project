c4d1ffe3ec8d57ef06ce71172c8f5a3bf2d81c4a
kareemiztolive <nelsonsmf@gmail.com>
2020-01-21 11:40:14 -0600

    pushed

--- anime-frontend-react/src/components/AnimeCard.js
+++ anime-frontend-react/src/components/AnimeCard.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 export default class AnimeCard extends Component {
+  state = {
+    buttonClicked: false
+  };
+
   render() {
     return (
       <div className="ui card">
@@ -9,14 +13,32 @@ export default class AnimeCard extends Component {
         <div className="content">
           <a className="header">{this.props.anime.title}</a>
           <a className="header">Year Released: {this.props.anime.seasonYear}</a>
-          <div className="header"> Number of Episodes: {this.props.anime.episodes}
+          <div className="header">
+            {" "}
+            Number of Episodes: {this.props.anime.episodes}
           </div>
         </div>
-            <div className="description">{}</div>
-       <div className="description">{}</div>
-        
-        <button className="ui blue button">Add to Watch List</button>
+        <div className="description">{}</div>
+        <div className="description">{}</div>
 
+        <div>
+          <button className="ui blue button">Add to Watch List</button>
+
+          <button
+            onClick={() =>
+              this.setState({ buttonClicked: !this.state.buttonClicked })
+            }
+            className="ui green button"
+          >
+            Description
+          </button>
+
+          {this.state.buttonClicked ? (
+            <div>
+                {this.props.anime.description}
+            </div>
+          ) : null}
+        </div>
       </div>
     );
   }
